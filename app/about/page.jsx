392473939878a9e535d189c97da48f0b7e9d2a11
mdmidulhosen <muhammadmridulhosenkibria@gmail.com>
2024-10-10 11:41:21 +0600

    next js initial setup done

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function page({ params }) {
+  console.log(params);
+  return (
+    <div>
+      <h1 className="font-roboto font-bold">this is my about page</h1>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary1: "#FBF9F1",
+        primary2: "#F6F2DD",
+        primary3: "#EFE7C2",
+        primary4: "#E7DCA6",
+        primary6: "#D9C771",
+        primary10: "#625A33",
+        neutral8: "#595959",
+        offBlack: "#00000073",
+      },
+      fontFamily: {
+        roboto: ["'Roboto', 'sans-serif'"],
+        abril: ["'Abril Fatface', 'serif'"],
+        merri: ["'Merriweather', 'serif'"],
       },
     },
   },
