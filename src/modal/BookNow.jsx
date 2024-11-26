1fe64b1308aea93cb3e7c64ee1acbd01ff30b603
abir <antorabir@gmail.com>
2024-11-26 18:21:42 +0600

    design: booking and tutorial

--- src/modal/BookNow.jsx
+++ src/modal/BookNow.jsx
@@ -129,13 +129,13 @@ function BookNow() {
 
   return (
     <>
-      <section id="book" className="py-20 bg-primary10">
+      <section id="book" className="py-20 bg-primary10 h-full">
         <div className="container mx-auto">
           <div>
             <h1 className="text-primary2 text-[48px] font-normal font-merri text-center">
               Book an Appointment
             </h1>
-            <p className="text-primary3 text-base leading-[30px] font-roboto font-normal text-center mt-2">
+            {/* <p className="text-primary3 text-base leading-[30px] font-roboto font-normal text-center mt-2">
               Connect with qualified, GMC-registered medical professionals from
               the <br />
               comfort of your home. For your convenience, we currently offer
@@ -145,7 +145,7 @@ function BookNow() {
               care, and seamless integration with NHS services. Book your
               appointment <br />
               today and discover the unparalleled benefits of MyDoctorClinic.
-            </p>
+            </p> */}
             <div className="text-center">
               <button
                 className="text-secondaryBlack bg-primary2 text-center font-merri text-sm py-2 px-6 rounded-sm font-normal mt-7"
